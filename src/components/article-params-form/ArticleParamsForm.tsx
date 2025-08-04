f58c3f2240b14e6d89083bb1e76eabe6bb14980d
Mariia Sunnikova <honey.born@mail.ru>
2025-08-04 20:51:49 +0300

    1st iteration review Developed form functional

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,174 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { Select } from 'src/ui/select/Select';
+import { Text } from 'src/ui/text';
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
+import { isFontFamilyClass } from 'src/ui/select/helpers/isFontFamilyClass';
 
-export const ArticleParamsForm = () => {
+export type pageStateOptions = {
+	fontFamily: OptionType;
+	fontSize: OptionType;
+	fontColor: OptionType;
+	bgColor: OptionType;
+	contentWidth: OptionType;
+};
+
+export const ArticleParamsForm = ({
+	propsState,
+	initialState,
+	onSubmitHandler,
+}: {
+	propsState: pageStateOptions;
+	initialState: pageStateOptions;
+	onSubmitHandler: (newState: pageStateOptions) => void;
+}) => {
+	//const initialPageState = { ...propsState };
+	const [newPageState, setNewPageState] = useState({ ...propsState });
+	const [formState, setFormState] = useState({ ...propsState });
+	const [isOpen, setIsOpen] = useState(false);
+
+	const asideRef = useRef<HTMLElement | null>(null);
+	//const arrowButtonRef = useRef<HTMLButtonElement>(null);
+	const arrowButtonRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (newPageState !== formState) {
+			setNewPageState({ ...formState });
+		}
+	}, [formState]);
+
+	useEffect(() => {
+		const asideCloseFunction = (evt: MouseEvent) => {
+			const eventElement = evt.target as Node;
+			const arrowButton = arrowButtonRef.current;
+
+			/* Проверяем, что пойманный клик был не по:
+			открытому элементу формы,
+			кнопки открытия / закрытия формы
+			элементу выпадающего списка в полях формы, которые формируются только в момент открытия списка и в исходном asideRef отсутствуют
+			*/
+			if (
+				asideRef &&
+				!asideRef.current?.contains(eventElement) &&
+				!(
+					arrowButton === eventElement ||
+					(arrowButton && arrowButton.contains(eventElement))
+				) &&
+				!eventElement.parentElement?.className.includes('Select-module')
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		if (isOpen) {
+			document.addEventListener('click', asideCloseFunction);
+		} else {
+			document.removeEventListener('click', asideCloseFunction);
+		}
+
+		return () => {
+			document.removeEventListener('click', asideCloseFunction);
+		};
+	}, [isOpen]);
+
+	const handleChangeInput = (option: OptionType) => {
+		if (option.className.includes('font-size')) {
+			setFormState({ ...formState, fontSize: option });
+		} else if (option.className.includes('font')) {
+			setFormState({ ...formState, fontColor: option });
+		} else if (option.className.includes('bg')) {
+			setFormState({ ...formState, bgColor: option });
+		} else if (option.className.includes('width')) {
+			setFormState({ ...formState, contentWidth: option });
+		} else if (isFontFamilyClass(option.className)) {
+			setFormState({ ...formState, fontFamily: option });
+		}
+	};
+
+	const handleOpenButton = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleResetButton = () => {
+		setFormState({ ...initialState });
+		onSubmitHandler(initialState);
+	};
+
+	const handleFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+		setIsOpen(false);
+		onSubmitHandler(newPageState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={handleOpenButton}
+				ref={arrowButtonRef}
+			/>
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, isOpen ? styles.container_open : '')}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
+					<Text as='h2' size={31} weight={800} uppercase align='left'>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamily}
+						options={fontFamilyOptions}
+						placeholder={formState.fontFamily.title}
+						onChange={handleChangeInput}
+						title={'шрифт'}
+					/>
+					<RadioGroup
+						name={'radio-buttons'}
+						options={fontSizeOptions}
+						selected={formState.fontSize}
+						title={'рАЗМЕР шрифта'}
+						onChange={handleChangeInput}
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						placeholder={formState.fontColor.title}
+						title={'Цвет шрифта'}
+						onChange={handleChangeInput}
+					/>
+					<Separator />
+					<Select
+						selected={formState.bgColor}
+						options={backgroundColors}
+						placeholder={formState.bgColor.title}
+						onChange={handleChangeInput}
+						title={'Цвет фона'}
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						placeholder={formState.contentWidth.title}
+						title={'Ширина контента'}
+						onChange={handleChangeInput}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleResetButton}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import {
+	ArticleParamsForm,
+	pageStateOptions,
+} from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
@@ -13,19 +16,42 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [style, setStyle] = useState({
+		'--font-family': defaultArticleState.fontFamilyOption.value,
+		'--font-size': defaultArticleState.fontSizeOption.value,
+		'--font-color': defaultArticleState.fontColor.value,
+		'--container-width': defaultArticleState.contentWidth.value,
+		'--bg-color': defaultArticleState.backgroundColor.value,
+	});
+
+	const initialState = {
+		fontFamily: defaultArticleState.fontFamilyOption,
+		fontSize: defaultArticleState.fontSizeOption,
+		fontColor: defaultArticleState.fontColor,
+		contentWidth: defaultArticleState.contentWidth,
+		bgColor: defaultArticleState.backgroundColor,
+	};
+
+	const [newState, setNewState] = useState({ ...initialState });
+
+	const handleSubmitButton = (newState: pageStateOptions) => {
+		setStyle({
+			'--font-family': newState.fontFamily.value,
+			'--font-size': newState.fontSize.value,
+			'--font-color': newState.fontColor.value,
+			'--container-width': newState.contentWidth.value,
+			'--bg-color': newState.bgColor.value,
+		});
+		setNewState({ ...newState });
+	};
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<main className={clsx(styles.main)} style={style as CSSProperties}>
+			<ArticleParamsForm
+				propsState={newState}
+				initialState={initialState}
+				onSubmitHandler={handleSubmitButton}
+			/>
 			<Article />
 		</main>
 	);
